602386d437077aa540f048479afa46e1b35c6451
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-04-29 12:16:27 +0200

    Konfiguracja bazy danych

--- src/Database.php
+++ src/Database.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 namespace App;
 
 use PDO;
+use Exception;
 
 class Database
 {
     public function __construct(array $config)
     {
+        dump($config);
         $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-        $connection = new PDO($dsn, $config['user'], $config['password']);
-        dump($connection);
+
+        try{
+            $connection = new PDO('sss');
+            dump($connection);
+        }catch (Exception $e){
+            dump($e);
+        }
+
+//        $connection = new PDO($dsn, $config['user'], $config['password']);
+//        dump($connection);
     }
 }

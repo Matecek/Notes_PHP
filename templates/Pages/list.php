31f1c9426a65641282f17eb8a2be19d6f8d80ecd
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-05-13 14:41:53 +0200

    Dodawanie notatki

--- templates/Pages/list.php
+++ templates/Pages/list.php
@@ -1,13 +1,32 @@
-<div>
-        <?php
-            if(!empty($params['before'])) {
+<div class="list">
+    <section>
+            <?php
+            if (!empty($params['before'])) {
                 switch ($params['before']) {
                     case 'created':
-                        echo '<div class="message">Notatka została utworzona</div>';
+                        echo '<div class="message">Notatka zostało utworzona</div>';
                         break;
                 }
             }
-        ?>
-	<h3>Lista notatek</h3>
-	<b><?php echo $params['resultList'] ?? ""; ?></b>
+            ?>
+
+        <div class="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Tytuł</th>
+                    <th>Opcje</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </section>
 </div>
--- templates/layout.php
+++ templates/layout.php
@@ -3,8 +3,10 @@
 <head>
 	<title>Notatnik</title>
 	<meta charset="utf-8">
+    <link rel="icon" href="/public/favicon.svg">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 	<link href="/public/style.css" rel="stylesheet">
+
 </head>
 
 <body class="body">
